8f8c6e5d3b9c90c38ca4bd0625eef32c8f301f96
Juraj Selep <jurajselep@gmail.com>
2017-10-07 20:00:02 +0200

    Added layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,  
 })
 export class AppComponent {
   title = 'app';
+  isLaunched = true;
+  fillerContent = Array(3);
+  fixed = true;
+  coverHeader = false;
+  showHeader = true;
+  showFooter = false;
+  modeIndex = 0;
+  get mode() { return ['side', 'over', 'push'][this.modeIndex]; }
+  get fixedTop() { return this.fixed && this.showHeader && !this.coverHeader ? 64 : 0; }
+  get fixedBottom() { return this.fixed && this.showFooter && !this.coverHeader ? 64 : 0; }
 }
