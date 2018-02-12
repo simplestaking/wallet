e71d4330ca873c096d4210373d3e4088fb02097e
Juraj Selep <jurajselep@gmail.com>
2018-02-12 21:58:53 +0100

    Added google analytics

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,18 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
+
+  // add Google Analytics script to <head>
+  const script = document.createElement('script');
+  script.innerHTML = `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-114065646-1');`;
+  document.head.appendChild(script);
+
   enableProdMode();
 }
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
