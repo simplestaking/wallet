e9da961d4ae0876e4955e1ae500e1ad3604375b4
Juraj Selep <jurajselep@gmail.com>
2018-09-25 19:12:49 +0200

    Added ofRoute rxjs type

---
+++ src/app/shared/utils/rxjs/operators.ts
@@ -0,0 +1,25 @@
+import { OperatorFunction } from 'rxjs/interfaces';
+import { Action } from '@ngrx/store';
+import { filter } from 'rxjs/operators';
+
+interface ActionWithPayload extends Action {
+    payload?: object;
+}
+
+// return true only if url match route
+export function ofRoute(route: string | string[]): OperatorFunction<Action, Action> {
+    return filter((action: any) => {
+
+        // process only ROUTER_NAVIGATION actions
+        if (action.type === 'ROUTER_NAVIGATION') {
+
+            // console.log('[ofRoute]', action.payload, route, action.payload.event.url, action.payload.event.url.indexOf(route) > 0)
+            if (action.payload.event.url.indexOf(route) > 0) {
+                return true
+            }
+
+        }
+        return false
+
+    });
+}
--- src/app/tezos/tezos-wallet/tezos-wallet-list/tezos-wallet-list.effect.ts
+++ src/app/tezos/tezos-wallet/tezos-wallet-list/tezos-wallet-list.effect.ts
@@ -5,8 +5,20 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import { of } from 'rxjs';
 import { map, withLatestFrom, flatMap, catchError } from 'rxjs/operators';
 
+import { ofRoute } from 'app/shared/utils/rxjs/operators';
+
+
 @Injectable()
 export class TezosWalletListEffects {
+
+
+    @Effect()
+    TezosWalletListLoad$ = this.actions$.pipe(
+        ofRoute('/tezos/wallet'),
+        
+        map(response => ({ type: 'TEZOS_BALANCE' }))
+    )
+
 
     constructor(
         private actions$: Actions,
